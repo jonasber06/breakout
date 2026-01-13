309f98c4d33631e5aaa444ba46e599f14f63dcb5
jonasber06 <144693393+jonasber06@users.noreply.github.com>
2026-01-13 17:14:26 +0100

    Oppdatering av BREAKOUT.py
    Endret en del av logikken
    
    blant annet at spillvinduet kan være oppe før spillet begynner

--- BREAKOUT.py
+++ BREAKOUT.py
@@ -1,14 +1,17 @@
 import pygame as pg
 import math as m
+from random import randint as r
 
-BREDDE = 900
-HOYDE = 700
+BREDDE = 700
+HOYDE = 450
+clock = pg.time.Clock()
 
-blokker = []
+
 
 class App:
     def __init__(self):
         self._seier = False
+        self._trykket = False
 
         """
         gjør følgende
@@ -29,28 +32,27 @@ class App:
         pg.display.set_caption(f"score: {self.score}")
 
     def kollisjon_paddle(self):
-        #print(self.paddle.xpos)
-        #print(self.ball.xpos1)
         x_rekt = self.paddle.xpos
         y_rekt = self.paddle.ypos
 
-        b = ((BREDDE-10*11)/10)+10
+        b = ((BREDDE-10*11)/10)+10 #høyde og bredde som paddle har
         h = ((HOYDE)/3 - 10*7)/6
 
         x_ball = self.ball.xpos1
         y_ball = self.ball.ypos1
+
         # finner nærmeste punkt på paddle til sirkelens radius
         self.x1 = max(x_rekt, min(x_ball,x_rekt+b))
         self.y1 = max(y_rekt, min(y_ball,y_rekt+h))
 
         avstand = m.sqrt((x_ball - self.x1)**2+(y_ball-self.y1)**2)
 
-        if avstand < self.ball.r:
-
+        #snur om på retningen slik at ballen 'spretter'
+        if avstand <= self.ball.r:
             self.ball.dy *= -1
-            self.ball.ypos1 -= self.ball.r // 2
 
     def kollisjon_blokk(self):
+        blokker = self.blokker._hent_blokker()
 
         for block in blokker:
 
@@ -67,21 +69,25 @@ class App:
             y1 = max(y_rekt, min(y_ball,y_rekt+h))
 
             avstand = m.sqrt((x_ball - x1)**2+(y_ball-y1)**2)
+
             if avstand <= self.ball.r:
                 self.ball.dy *= -1
-                block[0] = 40000
-                block[1] = 40000 #flytter blokken som har kollidert med ballen langt utenfor skjermen, slik at den "forsvinner"
+
                 self.score += 1
                 print(f"score: {self.score}")
                 pg.display.set_caption(f"score: {self.score}")
 
-                if self.score == 10:
-                    self.ball.dx *= 1.5
-                    self.ball.dy *= 1.5
+                self.blokker.fjern_blokk(block)
+
+                #øker vanskelighetsgraden
+                if self.score == 15:
+                    self.ball.dx *= 1.3
+                    self.ball.dy *= 1.3
                 
+                #60 poeng tilsvarer at alle blokkene er borte, og brettet er tomt.
                 if self.score == 60:
                     self._seier = True
-    
+
     def vunnet(self):
         #når alle blokkene er borte kjøres denne metoden, og spillet stoppes
         self.ball.dx = 0
@@ -97,6 +103,13 @@ class App:
         self.blokker.render(vindu)
         self.paddle.render(vindu)
         self.ball.render(vindu)
+
+        #tekstinstruks for å starte spillet
+        if not self._trykket:
+            font = pg.font.SysFont(None,25)
+            text = font.render(f'Trykk på piltast til høyre/venstre', True, (0, 0, 0))        
+            self.vindu.blit(text, (BREDDE // 2 -125 , HOYDE // 2+50))
+
         pg.display.flip()
 
     def run(self): 
@@ -108,8 +121,13 @@ class App:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-            
-            
+
+
+            #slik begynner ikke spillet før spilleren har trykket på en av pilene
+            if not self._trykket:
+                if self.paddle._sjekk_trykket():
+                    self.ball._begynn_bevegelse()
+                    self._trykket = True
 
             self.kollisjon_blokk()
             self.paddle.bevegelse()
@@ -117,49 +135,60 @@ class App:
             self.kollisjon_paddle()
             self.ball.beveg()
 
-            
-
+            #dersom spilleren ikke klarer å holde ballen oppe
             if self.ball.ypos1 >= HOYDE:
                 running = False
                 print("game over") 
-                          
-            self.render(self.vindu)
-
+            
+            self.render(self.vindu) #oppdatering
+                        
             if self._seier:
+                #når spillet er vunnet, stoppes spill-loop, og det vises en gratulasjon på skjermen
                 running = False
                 self.vunnet()
+            
+
+            clock.tick(60) #60 fps
 
 
 class Blokker:
+    def __init__(self):
+        self._blokker = []
+    
     def lag_blokker(self):
-        """
-        genererer liste med [x,y] koordinater for blokkene
-        """
-        m = 10
-        self.b = (BREDDE-m*11)/10
-        self.h = ((HOYDE)/3 - m*7)/6
 
-        for i in range(0,10):
-            x = (i+1)*m+self.b*i
-            for i in range(0,6):
-                y = (i+1)*m+self.h*i
-                blokker.append([x,y])
+        #genererer liste med [x,y] koordinater for blokkene
+
+        ant = 10                      
+        self.b = (BREDDE-ant * 11) / 10
+        self.h = ((HOYDE)/3 - ant * 7) / 6 #hoyde / 3, siden kun den øverste tredjedelen av skjermen skal ha blokker.
+
+        for i in range(0,ant):       #kolonner
+            x = (i+1) * ant + self.b * i
+            for k in range(0,6):    #rader
+                y = (k+1) * ant + self.h * k
+                self._blokker.append([x,y])
+
 
     def render(self,vindu):
-        """
-        tegner det som er i listen av blokkenes posisjoner
-        Gjør det mulig å fjerne blokker fra listen
+
+        #tegner det som er i listen av blokkenes posisjoner
+        #muliggjør også fjerning av blokker når de kollideres med.
         
-        """
-        for blokk in blokker:
+        for blokk in self._blokker:
             x = blokk[0]
             y = blokk[1]
 
-
             pg.draw.rect(vindu,(0,255,0),pg.Rect(x,y,self.b,self.h))
+    
+    def _hent_blokker(self):
+        return self._blokker
+    
+    def fjern_blokk(self,el):
+        self._blokker.remove(el)
 
 class Paddle:
-    def __init__(self, farge = (255,0,0), ypos = HOYDE - 50, fart = 1):
+    def __init__(self, farge = (255,0,0), ypos = HOYDE - 50, fart = 7.5):
         b = ((BREDDE-10*11)/10)+10
         self.ypos = ypos
         self.xpos = BREDDE // 2 - b/2
@@ -176,6 +205,11 @@ class Paddle:
         if keys[pg.K_LEFT] and self.xpos > 0:
             self.xpos -= self.fart
 
+    def _sjekk_trykket(self):
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_RIGHT] or keys[pg.K_LEFT]:
+            return True
     
     def render(self,vindu):
         b = ((BREDDE-10*11)/10)+10
@@ -183,7 +217,7 @@ class Paddle:
         pg.draw.rect(vindu,self.farge,pg.Rect(self.xpos,self.ypos,b,h))
     
 class Ball:
-    def __init__(self, xpos1 = BREDDE // 2, ypos1 = HOYDE // 2, farge = (0,0,255), r = 10, dx = 0.5, dy = 0.5):
+    def __init__(self, xpos1 = BREDDE // 2, ypos1 = HOYDE // 2, farge = (0,0,255), r = 10, dx = 0, dy = 0):
         self.xpos1 = xpos1
         self.ypos1 = ypos1
         self.farge = farge
@@ -205,8 +239,18 @@ class Ball:
     def kollisjon_paddle(self):
             self.dy *= -1
 
+    def _begynn_bevegelse(self):
+        self.dy = 4
+        self.dx = 4 * (-1) ** r(1,2) #randint, slik at det er tilfeldig hvilken retning ballen begynner
+
+    def _sjekk_bevegelse(self):
+        if self.dy == 0:
+            return False
+        return True
+
     def render(self,vindu):
         pg.draw.circle(vindu,self.farge,(self.xpos1,self.ypos1),self.r)
 
 a = App()
 a.run()
+
